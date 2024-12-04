43bc69089c2be171a257e956e074c58e3380f34a
KatherineRincon <ingry_krincon@soy.sena.edu.co>
2024-12-04 17:10:38 -0500

    evidencia de Django programmers

--- api/models.py
+++ api/models.py
@@ -10,4 +10,7 @@ class programmer(models.Model):
     Cellphone = models.CharField(max_length=10, null=True, default=None)
     is_active =models.BooleanField(default=True)
     
+class Student(models.Model):
+    name = models.CharField(max_length)    
+    
     
